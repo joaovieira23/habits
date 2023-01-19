2f51154e5bcd8cfe4c1d95c8036312d649ae995f
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-19 10:31:09 -0300

    chore: isolate backend files

---
+++ server/src/routes.ts
@@ -0,0 +1,11 @@
+import { prisma } from "./lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function appRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const habits = await prisma.habit.findMany()
+    
+        return habits;
+    });
+}
+ 
