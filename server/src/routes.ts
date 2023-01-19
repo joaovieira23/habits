7a19a25728899f0979850d99228f45380d866ed5
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-19 11:27:07 -0300

    feat: day details route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -31,5 +31,51 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
     });
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        });
+
+        const { date } = getDayParams.parse(request.query);
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        // Todos os hábitos possíveis naquele dia
+        // Hábitos que já foram completados
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        });
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true,
+            }
+        });
+
+        const completedHabits = day?.dayHabits.map((dayHabit) => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits
+        };
+
+    })
 }
  
