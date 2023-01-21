0f340bf11ecf1afee6d7cd1c7844e351f475908b
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-21 10:56:34 -0300

    chore: client http

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
+import { useState } from "react";
 
 const availableWeekDays = [
     'Domingo',
@@ -12,6 +13,21 @@ const availableWeekDays = [
 ];
 
 export function NewHabitForm() {
+
+    const [title, setTitle] = useState('');
+    const [weekDays, setWeekDays] = useState<number[]>([]);
+
+    function handleToggleWeekDay(weekDay: number) {
+        if(weekDays.includes(weekDay)) {
+            const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+
+            setWeekDays(weekDaysWithRemovedOne)
+        } else {
+            const weekDaysWithAddedOne = [...weekDays, weekDay];
+            setWeekDays(weekDaysWithAddedOne);
+        }
+    }
+
     return (
         <form className="w-full flex flex-col mt-6">
             <label htmlFor="title" className="font-semibold leading-tight">
@@ -31,9 +47,13 @@ export function NewHabitForm() {
             </label>
 
             <div className="flex flex-col gap-2 mt-3">
-                {availableWeekDays.map((weekDay) => {
+                {availableWeekDays.map((weekDay, index) => {
                     return (
-                        <Checkbox.Root key={weekDay} className='flex items-center gap-3 group'>
+                        <Checkbox.Root 
+                            key={weekDay} 
+                            className='flex items-center gap-3 group'
+                            onCheckedChange={() => handleToggleWeekDay(index)}
+                        >
                             <div className='h-8 w-8 roundend-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
                                 <Checkbox.CheckboxIndicator>
                                 <Check size={20} className="text-white" />
