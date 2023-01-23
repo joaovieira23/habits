35a4c986580d0a8ecdc6d4a3e6af2f835d351416
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-23 12:23:45 -0300

    feat: sync complete habits

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import { api } from '../lib/axios';
 
 interface HabitsListProps {
     date: Date;
+    onCompletedChange: (completed: number) => void;
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
     completedHabits: string[]
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChange }: HabitsListProps) {
     const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
     useEffect(() => {
@@ -30,7 +31,36 @@ export function HabitsList({ date }: HabitsListProps) {
         });
     }, []);
 
-    const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
+    async function handleToggleHabit(habitId: string) {
+        await api.patch(`/habits/${habitId}/toggle`);
+
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+        let completedHabits: string[] = [];
+
+        if(isHabitAlreadyCompleted) {
+            // remover da lista
+
+            completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+        } else {
+            // adicionar na lista
+            
+            completedHabits = [...habitsInfo!.completedHabits, habitId];
+        }
+
+
+        setHabitsInfo({
+            possibleHabits: habitsInfo!.possibleHabits,
+            completedHabits,
+        });
+
+        onCompletedChange(completedHabits.length);
+
+    }
+
+    const isDateInPast = dayjs(date)
+        .endOf('day')
+        .isBefore(new Date());
 
     return (
                     <div className="mt-6 flex flex-col gap-3">
@@ -38,6 +68,7 @@ export function HabitsList({ date }: HabitsListProps) {
                             return (
                                 <Checkbox.Root 
                                     key={habit.id}
+                                    onCheckedChange={() => handleToggleHabit(habit.id)}
                                     disabled={isDateInPast} 
                                     checked={habitsInfo.completedHabits.includes(habit.id)} 
                                     className='flex items-center gap-3 group'
