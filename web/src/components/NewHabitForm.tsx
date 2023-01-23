c0a6671963b0d4a25a71fcac7732a40a837df80e
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-23 12:55:40 -0300

    feat: finalize front end

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -58,7 +58,7 @@ export function NewHabitForm() {
                 onChange={(event) => setTitle(event.target.value)}
                 id="title"
                 placeholder="ex.: Exercícios, dormir bem, etc..."
-                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}
             />
@@ -73,10 +73,10 @@ export function NewHabitForm() {
                         <Checkbox.Root 
                             key={weekDay} 
                             checked={weekDays.includes(index)}
-                            className='flex items-center gap-3 group'
+                            className='flex items-center gap-3 group focus:outline-none'
                             onCheckedChange={() => handleToggleWeekDay(index)}
                         >
-                            <div className='h-8 w-8 roundend-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                            <div className='h-8 w-8 roundend-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                                 <Checkbox.CheckboxIndicator>
                                 <Check size={20} className="text-white" />
                                 </Checkbox.CheckboxIndicator>
@@ -90,7 +90,10 @@ export function NewHabitForm() {
                 })}
             </div>
 
-            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+            <button 
+                type="submit" 
+                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+            >
                 <Check size={20} weight="bold" />
                 Confirmar
             </button>
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -10,7 +10,7 @@ export function ProgressBar({ progress }: ProgressBarProps) {
                             aria-label="Progresso de hábitos completados nesse dia"
                             aria-valuemin={0}
                             aria-valuenow={progress}
-                            className='h-3 rounded-xl bg-violet-600'
+                            className='h-3 rounded-xl bg-violet-600 transition-all'
                             style={{ width: `${progress}%`}}
                         />
                     </div>
